b0a6a975bbc77ea813ecda73c8ef268013a1ec5b
mellowp <mellow_p@naver.com>
2019-09-24 18:56:52 +0900

    # [REST-API] H2 및 JPA 기본 연동 개발

--- src/main/java/com/mellowp/spring/service/SpringWebServiceApplication.java
+++ src/main/java/com/mellowp/spring/service/SpringWebServiceApplication.java
@@ -2,9 +2,13 @@ package com.mellowp.spring.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringWebServiceApplication {
+
+    // TODO: @EnableJpaAuditing -> JPA Auditing 활성화
 
     public static void main(String[] args) {
         SpringApplication.run(SpringWebServiceApplication.class, args);
