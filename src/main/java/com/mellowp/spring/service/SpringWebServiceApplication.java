a6d78e392e563472d4cd7464941437095fd636a7
mellowp <mellow_p@naver.com>
2019-09-24 13:28:05 +0900

    # Sptring Boot 2.1.8 + jpa + h2 기반 프로젝트 생성

---
+++ src/main/java/com/mellowp/spring/service/SpringWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mellowp.spring.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebServiceApplication.class, args);
+    }
+
+}
